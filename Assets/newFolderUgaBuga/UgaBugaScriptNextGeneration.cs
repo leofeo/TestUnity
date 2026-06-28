a38b6525fae4f443d65c1eed15e039daa436ba2c
leofeo <dzirt3d@gmail.com>
2026-06-28 09:09:15 +0700

    жарко

--- Assets/newFolderUgaBuga/UgaBugaScriptNextGeneration.cs
+++ Assets/newFolderUgaBuga/UgaBugaScriptNextGeneration.cs
@@ -5,13 +5,13 @@ public class UgaBugaScriptNextGeneration : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for(int i =0; i < 10; i++)
+            Debug.Log("уга буга");
     }
 
     // Update is called once per frame
     void Update()
     {       
-        for(int i =0; i < 10; i++)
-            Debug.Log("уга буга");
+        
     }
 }
