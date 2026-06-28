8ccab7970ecf612ee788ddb72b5e2a689da523ef
leofeo <dzirt3d@gmail.com>
2026-06-28 11:40:00 +0700

    idon't know what is that

--- Assets/newFolderUgaBuga/UgaBugaScriptNextGeneration.cs
+++ Assets/newFolderUgaBuga/UgaBugaScriptNextGeneration.cs
@@ -1,12 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UgaBugaScriptNextGeneration : MonoBehaviour
 {
+    private string gmo;
+    private List<int> _arr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _arr = new List<int>();
         for(int i =0; i < 10; i++)
-            Debug.Log("уга буга");
+            _arr.Add(i);
+
+        foreach(int arr in _arr)
+            gmo += $"- это цифра {arr}";
+
+        Debug.Log(gmo);
     }
 
     // Update is called once per frame
